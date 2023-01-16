c575dd3dd06ae48dc846c96631a018d17abe8d23
magatdarwin <magat.darwin@gmail.com>
2023-01-16 18:11:05 +0800

    Implement getValue

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ function multiply(firstNumber, secondNumber) {
     return Number(firstNumber) * Number(secondNumber);
 }
 
+function operate(operation, firstNumber, secondNumber) {
+    if (typeof window[operation] === 'function') {
+        return window[operation](firstNumber, secondNumber);
+    }
+}
+
 function divide(firstNumber, secondNumber) {
     if(Number(secondNumber) === 0) {
         alert('Who hurt you?');
@@ -19,9 +25,28 @@ function divide(firstNumber, secondNumber) {
     return  Number(firstNumber) / Number(secondNumber);
 }
 
-// function operate(operation, firstNumber, secondNumber) {
-//     return operation(firstNumber, secondNumber);
-// }
+function getValue(event) {
+    const input = event.target.value;
 
-const operate = (operation, firstNumber, secondNumber) => window[operation](firstNumber, secondNumber);
+    if (!isNaN(Number(input))) {
+        return Number(input);
+    }
+    else {
+        return input;
+    }
+}
 
+const OPERATORS = ['add', 'subtract', 'multiply', 'divide'];
+
+let ongoing = '';
+let firstNumber = 0;
+let secondNumber = 0;
+
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(button => button.addEventListener('click', event => {
+    console.log(getValue(event));
+}))
+
+
+// Temporary
+window.addEventListener('keydown', e => console.log(e.key));
